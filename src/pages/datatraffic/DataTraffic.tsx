490cf6b62e00ac5587424c64dd97b4f603619577
Cauba0201 <91552618+Cauba0201@users.noreply.github.com>
2024-06-25 14:29:23 +0700

    feature/data traffic

--- src/pages/datatraffic/DataTraffic.tsx
+++ src/pages/datatraffic/DataTraffic.tsx
@@ -1,9 +1,83 @@
-import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { tokens } from "../../theme";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
+import Header from "../../components/common/Header";
+import { mockDataContacts } from "../../data/mockData";
 
 type Props = {};
+interface RowData {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  phone: string;
+  address: string;
+  city: string;
+  zipCode: string;
+  registrarId: number;
+}
 
 const DataTraffic = (props: Props) => {
-  return <div>DataTraffic</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const columns: GridColDef<RowData>[] = [
+    { field: "id", headerName: "ID" },
+    { field: "registrarId", headerName: "Registrar ID" },
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      cellClassName: "name-column--cel",
+    },
+
+    {
+      field: "age",
+      headerName: "Age",
+      flex: 1,
+      type: "number",
+      headerAlign: "left",
+      align: "left",
+    },
+    {
+      field: "phone",
+      headerName: "Phone Number",
+      flex: 1,
+    },
+    {
+      field: "email",
+      headerName: "Email",
+      flex: 1,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      flex: 1,
+    },
+    {
+      field: "city",
+      headerName: "City",
+      flex: 1,
+    },
+    {
+      field: "zipCode",
+      headerName: "Zip Code",
+      flex: 1,
+    },
+  ];
+  return (
+    <Box>
+      <Header
+        title="Contact Information"
+        subtitle="List of contacts for Future Reference"
+      />
+      <Box>
+        <DataGrid
+          rows={mockDataContacts}
+          columns={columns}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default DataTraffic;
